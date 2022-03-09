a73d033ff8e4264c8c8f193e9598cc84c3ad6173
Samuel França <samuelhenriques2000@gmail.com>
2022-03-08 22:51:21 -0300

    Update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')
+                ->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')
+                ->nullable();
+            $table->string('phone_number');
+            $table->boolean('is_marketing')
+                ->default(0);
             $table->timestamps();
+            $table->timestamp('last_login_at')
+                ->nullable();
         });
     }
 
