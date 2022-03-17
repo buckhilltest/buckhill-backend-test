49ad7838c3c86efdacb12b9a6fb449bcdb167394
Samuel França <samuelhenriques2000@gmail.com>
2022-03-16 23:48:03 -0300

    Implement Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'uuid',
+        'price',
+        'description',
+        'metadata'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2'
+    ];
+
+    public function getMetadataAttribute($value)
+    {
+        return json_decode($value);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
