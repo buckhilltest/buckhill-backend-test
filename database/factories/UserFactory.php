c67bb9753ce0b511fb680672f5bf57b5833eb542
Samuel França <samuelhenriques2000@gmail.com>
2022-03-16 23:49:30 -0300

    Change UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,13 @@ class UserFactory extends Factory
             'first_name' => $this->faker->name(),
             'last_name' => $this->faker->name(),
             'is_admin' => false,
+            'avatar' => $this->faker->uuid(),
+            'is_marketing' => $this->faker->boolean(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2a$12$H67YfGvV84BwWP9GEEPiR.ktHQSU2i9/7u98TKDgSCCB7GONiVy1y', // userpassword
-            'phone_number' => $this->faker->phoneNumber()
+            'phone_number' => $this->faker->phoneNumber(),
+            'last_login_at' => $this->faker->dateTime(),
         ];
     }
 
