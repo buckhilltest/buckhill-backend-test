d58fdcaa27d8716690a8bb33fddd619b97575c4e
Samuel França <samuelhenriques2000@gmail.com>
2022-03-16 23:48:27 -0300

    Add Product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::factory()->create()->id,
+            'title' => $this->faker->title(),
+            'uuid' => $this->faker->uuid(),
+            'price' => $this->faker->randomFloat(2, 0, 9999999999),
+            'description' => $this->faker->text(),
+        ];
+    }
+}
