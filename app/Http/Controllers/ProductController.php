2f8c9c0e7e48f96e19e42f075c6e93439604241c
Samuel França <samuelhenriques2000@gmail.com>
2022-03-16 23:48:16 -0300

    Implement Product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,63 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProduct;
+use App\Http\Requests\UpdateProduct;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $products = Product::all();
+
+        return response()->json($products);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  StoreProduct  $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(StoreProduct $request): JsonResponse
     {
-        //
+        $data = $request->validated();
+
+        $product = Product::create($data);
+
+        return response()->json($product, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param  string  $uuid
+     * @return JsonResponse
      */
-    public function show(Product $product)
+    public function show(string $uuid): JsonResponse
     {
-        //
+        $product = Product::where('uuid', $uuid)
+            ->firstOrFail();
+
+        return response()->json($product);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param  UpdateProduct  $request
+     * @param  string  $uuid
+     * @return JsonResponse
      */
-    public function update(Request $request, Product $product)
+    public function update(UpdateProduct $request, string $uuid): JsonResponse
     {
-        //
+        $data = $request->validated();
+
+        $product = Product::where('uuid', $uuid)
+            ->firstOrFail();
+
+        if ($product->update($data)) {
+            return response()->json($product);
+        }
+
+        return response()->json([
+            'message' => 'Error updating product',
+        ], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param  string  $uuid
+     * @return JsonResponse
      */
-    public function destroy(Product $product)
+    public function destroy(string $uuid): JsonResponse
     {
-        //
+        $product = Product::where('uuid', $uuid)
+            ->firstOrFail();
+
+        if ($product->delete()) {
+            return response()->json([
+                'message' => 'Product deleted successfully',
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Error deleting product',
+        ]);
     }
 }
